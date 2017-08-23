33fa5ba5633c08700354ef5ea1d67af22ad5305f
YakseeHyd <suresh@yaksee.com>
2017-08-23 12:26:00 +0530

    Added code for hiding the app icon in App drawer

--- app/src/main/java/com/sharath/my_cbr/MainActivity.java
+++ app/src/main/java/com/sharath/my_cbr/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sharath.my_cbr;
 
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hideAppIcon();    //Disabled for now
+    }
+
+
+    private void hideAppIcon(){
+
+        PackageManager packageManager =  getPackageManager();
+        ComponentName cmpName = new ComponentName(this,com.sharath.my_cbr.MainActivity.class);
+        packageManager.setComponentEnabledSetting(cmpName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED,PackageManager.DONT_KILL_APP);
+
+    }
+
+    private void unHideAppIcon(){
+
+        PackageManager packageManager =  getPackageManager();
+        ComponentName cmpName = new ComponentName(this,com.sharath.my_cbr.MainActivity.class);
+        packageManager.setComponentEnabledSetting(cmpName,PackageManager.COMPONENT_ENABLED_STATE_ENABLED,PackageManager.DONT_KILL_APP);
+
+
     }
 }
